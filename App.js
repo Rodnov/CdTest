73e5800d788969315ae7183999f3ad7bd5642376
Rodnov <4096_88@mail.ru>
2022-02-09 20:41:08 +0700

    09022022 2041

--- App.js
+++ App.js
@@ -54,11 +54,40 @@ const App: () => Node = () => {
             </View>
           </View>
           {/* Fasting tracker */}
-          <View style={[styles.container, {marginTop: 20}]}>
-            <Image
-              style={{width: '100%'}}
-              source={require('./src/assets/images/Banner.png')}
-            />
+          <View
+            style={[
+              styles.container,
+              {
+                width: '100%',
+                marginTop: 20,
+                height: 112,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'flex-start',
+                backgroundColor: '#7573F0',
+                borderRadius: 16,
+              },
+            ]}>
+            <View style={{flexDirection:'row', justifyContent: 'space-between',
+                width:'100%'}}>
+              <View style={{paddingTop: 20, paddingLeft: 20}}>
+                <Text style={styles.bannerTitle}>Fasting tracker</Text>
+                <Text style={[styles.bannersubTitle, {marginTop: 6}]}>
+                  Start losing weight with interval
+                </Text>
+                <Text style={styles.bannersubTitle}>fasting</Text>
+              </View>
+              <View style={{paddingTop: 26, paddingRight: 23}}>
+                <IconMoon color={'white'} size={14} name="close" />
+              </View>
+            </View>
+
+            <View style={{position: 'absolute', right: 0}}>
+              <Image
+                style={{width: 170}}
+                source={require('./src/assets/images/Group565.png')}
+              />
+            </View>
           </View>
 
           {/* Progress today */}
@@ -71,7 +100,12 @@ const App: () => Node = () => {
                 alignItems: 'center',
               }}>
               <Text style={styles.title}> Progress today </Text>
-              <Text style={styles.details}> details </Text>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Text style={styles.details}> details </Text>
+                <IconMoon color={'#070934'} size={12} name="up" />
+              </TouchableOpacity>
             </View>
             <TouchableOpacity
               onPress={() => {}}
@@ -114,7 +148,12 @@ const App: () => Node = () => {
                 alignItems: 'center',
               }}>
               <Text style={styles.title}> My personal insights </Text>
-              <Text style={styles.details}> see all </Text>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Text style={styles.details}> see all </Text>
+                <IconMoon color={'#070934'} size={12} name="up" />
+              </TouchableOpacity>
             </View>
             <ScrollView
               horizontal={true}
@@ -143,24 +182,45 @@ const App: () => Node = () => {
               height: 70,
               width: '100%',
               backgroundColor: 'white',
-              justifyContent: 'center',
+              justifyContent: 'space-around',
               alignItems: 'center',
               position: 'absolute',
               bottom: 32,
               borderRadius: 32,
+              flexDirection: 'row',
             }}>
-            <View>
-
-               <IconMoon color={'white'} size={24} name="Home" />
-
-              <Text style={{color: '#070934'}}>main</Text>
-            </View>
-            <View>
-              <Text>+</Text>
-            </View>
-            <View>
-              <Text style={{color: '#A9AAB8'}}>journal</Text>
-            </View>
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginLeft: 9,
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <IconMoon color={'#070934'} size={16} name="Home" />
+              <Text style={[styles.tabTextActive, {marginLeft: 9}]}>main</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: '#7573F0',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 48,
+                width: 48,
+                height: 48,
+              }}>
+              <IconMoon color={'white'} size={24} name="plus" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginLeft: 9,
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <IconMoon color={'#A9AAB8'} size={18} name="journal" />
+              <Text style={styles.tabTextNotActive}>journal</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -181,6 +241,18 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     color: '#858692',
   },
+  bannerTitle: {
+    fontSize: 18,
+    fontFamily: 'Manrope-VariableFont_wght',
+    fontWeight: '700',
+    color: 'white',
+  },
+  bannersubTitle: {
+    fontSize: 16,
+    fontFamily: 'Manrope-VariableFont_wght',
+    fontWeight: '400',
+    color: 'white',
+  },
   container: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -196,6 +268,18 @@ const styles = StyleSheet.create({
     fontFamily: 'Manrope-VariableFont_wght',
     fontWeight: '500',
     color: '#070934',
+  },
+  tabTextActive: {
+    fontSize: 12,
+    fontFamily: 'Manrope-VariableFont_wght',
+    fontWeight: '500',
+    color: '#070934',
+  },
+  tabTextNotActive: {
+    fontSize: 12,
+    fontFamily: 'Manrope-VariableFont_wght',
+    fontWeight: '500',
+    color: '#A9AAB8',
   },
 });
 
